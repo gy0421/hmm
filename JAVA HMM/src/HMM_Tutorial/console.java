9394961d73f91e125f58a7c42af6d7c57067f17f
ogy <geunyoung.oh@oracle.com>
2018-09-18 09:34:20 +0900

    utf-8

---
+++ JAVA HMM/src/HMM_Tutorial/console.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+/**
+ * @author GEUNYOUNG OH
+ *
+ */
+package HMM_Tutorial;
+
+public class console {
+	public static void main(String[] args) {
+		System.out.println("GY");
+		System.out.prin	tln("Geunyoung");
+		System.out.println("GEUNYOUNG OH");
+		System.out.println("d0918");
+	}
+}
